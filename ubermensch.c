e3d68df40419fed61bbd35d2d43005ca7cd513a6
jardimdanificado <jardeltorreslima01@gmail.com>
2025-10-06 21:29:28 -0300

    some more changes?

--- ubermensch.c
+++ ubermensch.c
@@ -17,109 +17,109 @@
 // version
 #define UB_VERSION "0.9.4"
 
-typedef intptr_t UbInt;
-typedef uintptr_t UbUInt;
+typedef intptr_t Int;
+typedef uintptr_t UInt;
 
-// UB use UbInt and UbFloat instead of int and float because we need to use always the pointer size for any type that might share the fundamental union type;
+// UB use Int and Float instead of int and float because we need to use always the pointer size for any type that might share the fundamental union type;
 // UB use a union as universal type, and UB is able to manipulate and use pointers direcly so we need to use the pointer size;
 #if INTPTR_MAX == INT64_MAX
-    typedef double UbFloat;
-    typedef uint32_t UbUHalf;
-    typedef int32_t UbHalf;
+    typedef double Float;
+    typedef uint32_t UHalf;
+    typedef int32_t Half;
 #else
-    typedef float UbFloat;
-    typedef uint16_t UbUHalf;
-    typedef int16_t UbHalf;
+    typedef float Float;
+    typedef uint16_t UHalf;
+    typedef int16_t Half;
 #endif
 
 #ifndef UB_DEFAULT_SIZE
     #define UB_DEFAULT_SIZE 0
 #endif
 
-typedef union UbValue
+typedef union Value
 {
     // all types depend on the size of the pointer
-    UbInt i;
-    UbFloat f;
-    UbUInt u;
+    Int i;
+    Float f;
+    UInt u;
     void* p;
     uint8_t b[sizeof(void*)];
-} UbValue;
+} Value;
 
-typedef struct UbList
+typedef struct List
 {
     // the real size
-    UbUHalf capacity;
+    UHalf capacity;
     // the current size
-    UbUHalf size;
-    // the data, it is a pointer to a UbValue array
-    UbValue *data;
-} UbList;
+    UHalf size;
+    // the data, it is a pointer to a Value array
+    Value *data;
+} List;
 
-typedef void (*Function)(UbList *stack);
+typedef void (*Function)(List *stack);
    
-// UbList functions   
+// List functions   
 // create a new list with the given size, if size is 0, it will be initialized with NULL data and then allocated when needed
-static inline UbList*            ub_new(UbInt size);
+static inline List*            ub_new(Int size);
 // free the list    
-static inline void               ub_free(UbList *list);
+static inline void               ub_free(List *list);
 // double the list capacity   
-static inline void               ub_double(UbList *list);
+static inline void               ub_double(List *list);
 // halve the list capacity   
-static inline void               ub_half(UbList *list);
+static inline void               ub_half(List *list);
 // push a value to the end of the list
-static inline void               ub_push(UbList *list, UbValue value);
+static inline void               ub_push(List *list, Value value);
 // unshift a value to the start of the list
-static inline void               ub_unshift(UbList *list, UbValue value);
+static inline void               ub_unshift(List *list, Value value);
 // insert a value at index i in the list
-static inline void               ub_insert(UbList *list, UbInt i, UbValue value);
+static inline void               ub_insert(List *list, Int i, Value value);
 // pop a value from the end of the list
-static inline UbValue            ub_pop(UbList *list);
+static inline Value            ub_pop(List *list);
 // shift a value from the start of the list
-static inline UbValue            ub_shift(UbList *list);
+static inline Value            ub_shift(List *list);
 // remove and return the value at index i in the list, shifting the rest of the list
-static inline UbValue            ub_remove(UbList *list, UbInt i);
+static inline Value            ub_remove(List *list, Int i);
 // fast remove a value at index i in the list, swapping it with the last element and popping it
-static inline UbValue            ub_fast_remove(UbList *list, UbInt i);
+static inline Value            ub_fast_remove(List *list, Int i);
 // swap two values in the list at indices i1 and i2
-static inline void               ub_swap(UbList *list, UbInt i1, UbInt i2);
+static inline void               ub_swap(List *list, Int i1, Int i2);
 // find the index of a value in the list, returns -1 if not found
-static inline UbInt              ub_find(const UbList *list, UbValue value);
+static inline Int              ub_find(const List *list, Value value);
 // reverse the list in place   
-static inline void               ub_reverse(UbList *list);
+static inline void               ub_reverse(List *list);
 // create a copy of the list, with the same capacity and size, but new data array
-static inline UbList*            ub_copy(const UbList *list);
+static inline List*            ub_copy(const List *list);
 // get a value at index i in the list, returns a value with i set to -1 if index is out of range
-static inline UbValue            ub_get(const UbList *list, UbInt i);
+static inline Value            ub_get(const List *list, Int i);
 // set a value at index i in the list, if index is out of range, it will print an error and exit
-static inline void               ub_set(UbList *list, UbInt i, UbValue value);
+static inline void               ub_set(List *list, Int i, Value value);
 // get the ub version   
 static inline const char*        ub_get_version(void);
 // arena   
-static inline void*              ub_alloc(UbList *arena, size_t size);
+static inline void*              ub_alloc(List *arena, size_t size);
 // ub representation
-static inline void               ub_interpret(UbList *context, const char* input_str, UbList* code, UbList* stack);
+static inline void               ub_interpret(List *context, const char* input_str, List* code, List* stack);
 
 // functions implementations
 // functions implementations
 // functions implementations
 // functions implementations
 
-static inline UbList *ub_new(UbInt size)
+static inline List *ub_new(Int size)
 {
-    UbList *list = (UbList*)malloc(sizeof(UbList));
+    List *list = (List*)malloc(sizeof(List));
     
     if (list == NULL)
     {
-        printf("UB_ERROR: failed to allocate memory for UbList\n");
+        printf("UB_ERROR: failed to allocate memory for List\n");
         exit(EXIT_FAILURE);
     }
     
-    list->data = (size == 0) ? NULL : (UbValue*)malloc((size_t)size * sizeof(UbValue));
+    list->data = (size == 0) ? NULL : (Value*)malloc((size_t)size * sizeof(Value));
     
     if (size > 0 && list->data == NULL)
     {
-        printf("UB_ERROR: failed to allocate memory for UbList data\n");
+        printf("UB_ERROR: failed to allocate memory for List data\n");
         exit(EXIT_FAILURE);
     }
     
@@ -129,17 +129,17 @@ static inline UbList *ub_new(UbInt size)
     return list;
 }
 
-static inline void ub_free(UbList *list)
+static inline void ub_free(List *list)
 {
     free(list->data);
     free(list);
 }
 
-static inline void ub_double(UbList *list)
+static inline void ub_double(List *list)
 {
-    UbValue *new_data = NULL;
+    Value *new_data = NULL;
     list->capacity = list->capacity == 0 ? 1 : list->capacity * 2;
-    new_data = (UbValue*)realloc(list->data, (size_t)list->capacity * sizeof(UbValue));
+    new_data = (Value*)realloc(list->data, (size_t)list->capacity * sizeof(Value));
     if (!new_data)
     {
         printf("UB_ERROR: failed to reallocate list data\n");
@@ -148,11 +148,11 @@ static inline void ub_double(UbList *list)
     list->data = new_data;
 }
 
-static inline void ub_half(UbList *list)
+static inline void ub_half(List *list)
 {
-    UbValue *new_data = NULL;
+    Value *new_data = NULL;
     list->capacity /= 2;
-    new_data = (UbValue*)realloc(list->data, (size_t)list->capacity * sizeof(UbValue));
+    new_data = (Value*)realloc(list->data, (size_t)list->capacity * sizeof(Value));
     if (!new_data)
     {
         printf("UB_ERROR: failed to reallocate list data\n");
@@ -166,7 +166,7 @@ static inline void ub_half(UbList *list)
     }
 }
 
-static inline void ub_push(UbList *list, UbValue value)
+static inline void ub_push(List *list, Value value)
 {
     if (list->size == list->capacity)
     {
@@ -176,19 +176,19 @@ static inline void ub_push(UbList *list, UbValue value)
     list->size++;
 }
 
-static inline void ub_unshift(UbList *list, UbValue value)
+static inline void ub_unshift(List *list, Value value)
 {
     if (list->size == list->capacity)
     {
         ub_double(list);
     }
-    memmove(&(list->data[1]), &(list->data[0]), (size_t)list->size * sizeof(UbValue));
+    memmove(&(list->data[1]), &(list->data[0]), (size_t)list->size * sizeof(Value));
     list->data[0] = value;
 
     list->size++;
 }
 
-static inline void ub_insert(UbList *list, UbInt i, UbValue value)
+static inline void ub_insert(List *list, Int i, Value value)
 {
     if (list->size == list->capacity)
     {
@@ -202,15 +202,15 @@ static inline void ub_insert(UbList *list, UbInt i, UbValue value)
     }
     else
     {
-        memmove(&(list->data[i + 1]), &(list->data[i]), (size_t)(list->size - i) * sizeof(UbValue));
+        memmove(&(list->data[i + 1]), &(list->data[i]), (size_t)(list->size - i) * sizeof(Value));
         list->data[i] = value;
         list->size++;
     }
 }
 
-static inline UbValue ub_pop(UbList *list)
+static inline Value ub_pop(List *list)
 {
-    UbValue ret = list->data[list->size - 1]; 
+    Value ret = list->data[list->size - 1]; 
     if (list->size == 0)
     {
         printf("UB_ERROR: cannot pop from empty list\n");
@@ -220,9 +220,9 @@ static inline UbValue ub_pop(UbList *list)
     return ret;
 }
 
-static inline UbValue ub_shift(UbList *list)
+static inline Value ub_shift(List *list)
 {
-    UbValue ret = list->data[0];
+    Value ret = list->data[0];
     if (list->size == 0)
     {
         printf("UB_ERROR: cannot shift from empty list\n");
@@ -230,15 +230,15 @@ static inline UbValue ub_shift(UbList *list)
     }
     else if (list->size > 1) 
     { 
-        memmove(&(list->data[0]), &(list->data[1]), (size_t)(list->size - 1) * sizeof(UbValue)); 
+        memmove(&(list->data[0]), &(list->data[1]), (size_t)(list->size - 1) * sizeof(Value)); 
     } 
     list->size--; 
     return ret;
 }
 
-static inline UbValue ub_remove(UbList *list, UbInt i)
+static inline Value ub_remove(List *list, Int i)
 {
-    UbValue ret = list->data[i]; 
+    Value ret = list->data[i]; 
 
     size_t elements_to_move = 0; 
     if (list->size == 0)
@@ -250,28 +250,28 @@ static inline UbValue ub_remove(UbList *list, UbInt i)
     
     /* Move data elements */ 
     memmove(&(list->data[i]), &(list->data[i + 1]), 
-            elements_to_move * sizeof(UbValue)); 
+            elements_to_move * sizeof(Value)); 
     list->size--; 
     
     return ret;
 }
 
-static inline UbValue ub_fast_remove(UbList *list, UbInt i)
+static inline Value ub_fast_remove(List *list, Int i)
 {
     ub_swap(list, i, list->size - 1);
     return ub_pop(list);
 }
 
-static inline void ub_swap(UbList *list, UbInt i1, UbInt i2)
+static inline void ub_swap(List *list, Int i1, Int i2)
 {
-    UbValue tmp = list->data[i1];
+    Value tmp = list->data[i1];
     list->data[i1] = list->data[i2];
     list->data[i2] = tmp;
 }
 
-static inline UbInt ub_find(const UbList *list, UbValue value) 
+static inline Int ub_find(const List *list, Value value) 
 {
-    for (UbInt i = list->size - 1; i >= 0; i--)
+    for (Int i = list->size - 1; i >= 0; i--)
     {
         if (list->data[i].i == value.i)
         {
@@ -282,41 +282,41 @@ static inline UbInt ub_find(const UbList *list, UbValue value)
     return -1;
 }
 
-static inline void ub_reverse(UbList *list)
+static inline void ub_reverse(List *list)
 {
-    for (UbInt i = 0; i < list->size / 2; i++)
+    for (Int i = 0; i < list->size / 2; i++)
     {
         ub_swap(list, i, list->size - i - 1);
     }
 }
 
-static inline UbList* ub_copy(const UbList *list)
+static inline List* ub_copy(const List *list)
 {
-    UbList *copy = ub_new(list->capacity);
+    List *copy = ub_new(list->capacity);
     
     if (copy == NULL)
     {
-        printf("UB_ERROR: failed to allocate memory for UbList copy\n");
+        printf("UB_ERROR: failed to allocate memory for List copy\n");
         exit(EXIT_FAILURE);
     }
     copy->size = list->size;
-    memcpy(copy->data, list->data, (size_t)copy->size * sizeof(UbValue));
+    memcpy(copy->data, list->data, (size_t)copy->size * sizeof(Value));
     
     copy->capacity = list->capacity;
 
     return copy;
 }
 
-static inline UbValue ub_get(const UbList *list, UbInt i)
+static inline Value ub_get(const List *list, Int i)
 {
     if (i < 0 || i >= list->size)
     {
-        return (UbValue){.i=-1}; // return -1 if index is out of range
+        return (Value){.i=-1}; // return -1 if index is out of range
     }
     return list->data[i];
 }
 
-static inline void ub_set(UbList *list, UbInt i, UbValue value)
+static inline void ub_set(List *list, Int i, Value value)
 {
     if (i < 0 || i >= list->size)
     {
@@ -331,7 +331,7 @@ static inline const char* ub_get_version(void)
     return UB_VERSION;
 }
 
-static inline void* ub_alloc(UbList* arena, size_t size)
+static inline void* ub_alloc(List* arena, size_t size)
 {
     size_t aligned_size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
     // we use arena->size as offset
@@ -343,46 +343,16 @@ static inline void* ub_alloc(UbList* arena, size_t size)
     arena->size += aligned_size;
     return ptr;
 }
-/*
+
 // if you want to return something, pass a stack, values will be there
 // if you do not provide a stack, a new one will be created and freed at the end
-static inline void ub_interpret(UbList *context, const char* input_str, UbList* _code, UbList* _stack)
+static inline void ub_interpret(List *context, const char* input_str, List* _code, List* _stack)
 {
-    UbList *code;
-    UbList *stack;
-    UbInt i = 0;
+    List *code;
+    List *stack;
+    Int i = 0;
     char* original_str = NULL;
 
-    // this will increment every label
-    int difference = 0;
-    if (_code == NULL)
-    {
-        code = ub_new(UB_DEFAULT_SIZE);
-        original_str = strdup(input_str); // Duplicate the input string to avoid modifying the original
-        char* token = strtok(original_str, "\n\t \r");
-        while (token != NULL)
-        {
-            // we will also process the labels here
-            if (token[0] == ':') // label
-            {
-                // we remove the label from the token
-                char* label = token + 1; // skip the first character
-                // we push the label as an integer (its position in the code)
-                ub_push_int(context, i - difference, label, UB_TYPE_ANY);
-                difference++;
-            }
-            else 
-            {
-                ub_push_pointer(code, token, NULL, UB_TYPE_BUFFER);
-            }
-            token = strtok(NULL, "\n\t \r");
-            i++;
-        }
-    }
-    else
-    {
-        code = _code;
-    }
 
     if (_stack == NULL)
     {
@@ -393,6 +363,23 @@ static inline void ub_interpret(UbList *context, const char* input_str, UbList* 
         stack = _stack;
     }
 
+    if (_code == NULL)
+    {
+        code = ub_new(UB_DEFAULT_SIZE);
+    }
+    else
+    {
+        code = _code;
+    }
+
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
+    // interpreting goes here
     // interpreting goes here
 
     if (_code == NULL) 
@@ -402,5 +389,5 @@ static inline void ub_interpret(UbList *context, const char* input_str, UbList* 
 
     free(original_str); // free the original string
 }
-*/
+
 #endif // ifndef UB_H macro
